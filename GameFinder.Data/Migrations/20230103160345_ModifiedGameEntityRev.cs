6c3de863d2a03104be43cb4f7048cc9c657fb4d2
Charlie Lipperd <lipperd@hotmail.com>
2023-01-03 13:06:52 -0500

    completion of CreateGameEndpoints

---
+++ GameFinder.Data/Migrations/20230103160345_ModifiedGameEntityRev.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameFinder.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifiedGameEntityRev : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Games");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- GameFinder.Services/Game/IGameService.cs
+++ GameFinder.Services/Game/IGameService.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameFinder.Models.GameModels;
 
 namespace GameFinder.Services.Game
 {
     public interface IGameService
     {
-        
+        Task<bool> CreateGameAsync(GameCreate model);
+        Task<bool> EditGameAsync(GameEdit request);
+        Task<bool> DeleteGameAsync(int gameId);
     }
 }
