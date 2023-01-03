ec9bcd21a45cdbfb1e325139e34ad03d1a3b7669
Adam <Guy McGee>
2023-01-03 17:36:05 -0500

    delted code that was breaking my additions

--- GameFinder.Services/Genre/GenreService.cs
+++ GameFinder.Services/Genre/GenreService.cs
@@ -7,6 +7,33 @@ namespace GameFinder.Services.Genre
 {
     public class GenreService : IGenreService
     {
-        
+        // 1. Bring in the database through dependency injection
+        private ApplicationDbContext _context;
+
+        // private readonly ApplicationDbContext _context;
+
+        public GenreService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+
+    public async Task<GameDetails> GetGenreByIdAsync(int gameId)
+    {
+        GameEntity game = await _context.Games.FindAsync(gameId);
+        if (game is null)
+        {
+            return null;
+        }
+        // manuly mapping a GameEntity Object to a GameDetails Object
+        return new GameDetails
+        {
+            Id = game.Id,
+            Title = game.Title,
+            Genre = game.Genre,
+            Description = game.Description,
+            GameSystems = game.GameSystems
+        };
+
     }
 }
